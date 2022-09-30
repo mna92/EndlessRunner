7c9fe53d7be96e9c55cc3c1e585d61428a968994
mna92 <51138084+mna92@users.noreply.github.com>
2022-09-30 17:44:55 +0330

    QuickUpdateVid19

--- EndlessRunner/Assets/Scripts/CrashEndGame.cs
+++ EndlessRunner/Assets/Scripts/CrashEndGame.cs
@@ -1,14 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CrashEndGame : MonoBehaviour
 {
+    public AudioSource crashSound;
+    public GameObject ledgeGenerator;
+    public GameObject playerObject;
+    public GameObject finalScore;
+    public GameObject splashBackground;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "PlayerObject")
+        if (other.tag == "CrashBox")
         {
-            Debug.Log("Crash");
+            crashSound.Play();
+            ledgeGenerator.SetActive(false);
+            playerObject.GetComponent<CharacterLocomotion>().enabled = false;
+            finalScore.GetComponent<Text>().text = "FINAL SCORE: " + ScoreCubeMonitor.scoreNumber;
+            ScoreCubeMonitor.scoreNumber = 0;
+            playerObject.transform.position = new Vector3(-2, 1, 1);
+            StartCoroutine(SplashEndGame());
+
         }
+    }
+
+    IEnumerator SplashEndGame()
+    {
+        splashBackground.SetActive(true);
+        splashBackground.GetComponent<Animator>().Play("BackgroundFadeIn");
+        yield return new WaitForSeconds(1f);
+        finalScore.SetActive(true);
     }
 }
